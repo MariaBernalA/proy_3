c0d9ddf2b6c1d195b39029b3531b1191beb979d0
mknjbhvh <camila1034_@hotmail.com>
2024-01-21 23:37:45 -0500

    Se realizan comentarios en el codigo

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 //Paquete importado
-require('colors');
+require('colors');//se importa el modulo colors para agregar color al texto
 
-const Math = require('./modules/math.js');
+const Math = require('./modules/math.js');//Se importa el módulo math.js utilizando la función require y se asigna a las constante Math
 // referencia el modulo math a traves de la constante math
-const math = require('./modules/math.js')
+const math = require('./modules/math.js')//Se importa el módulo math.js utilizando la función require y se asigna a las constante math
 
-console.clear();
+console.clear();//se utiliza para limpiar la consola antes de imprimir cualquier salida.
 console.log(math);
 
 const main = async() => {
     console.log('*************************'.bgMagenta);
-    console.log('*  '.bgMagenta + 'Funciones Matematicas'.bgMagenta +  '  *'.bgMagenta);
+    console.log('*  '.bgMagenta + 'Funciones Matematicas'.bgMagenta +  '  *'.bgMagenta);//encabezado que se mostrara con dacoracion y colores en la consola
     console.log('************************\n'.bgMagenta);
     
     console.log('*********************************'.bgBlue);
-    console.log(' *'.bgBlue +  'Suma =>' + math.add(5,3) + '*****'.bgBlue);
+    console.log(' *'.bgBlue +  'Suma =>' + math.add(5,3) + '*****'.bgBlue);//Se realizan operaciones matemáticas utilizando las funciones importadas de math
     console.log(' *'.bgBlue +  'Resta =>' + math.substract(2,8) + '*********'.bgBlue);
     console.log(' *'.bgBlue +  'Multiplicacion =>' + math.multiply(2,8) + '*******'.bgBlue);
     console.log(' *'.bgBlue +  'Dividicion =>' + math.divide(3,2) + '**'.bgBlue);
     console.log('*********************************'.bgBlue);
 }
 
-main();
+main();//se cierra la funcion math para ejecutar el codigo en ella
--- modules/math.js
+++ modules/math.js
@@ -1,29 +1,29 @@
-const Math = {};
+const Math = {};//se declara una variable que se utiliza para agrupar las funciones relacionadas con operaciones matemáticas.
 
-function add(a,b) {
+function add(a,b) {//función llamada add que toma dos parámetros para sumar
     return a + b;
 }
 
-function substract(a,b) {
+function substract(a,b) {//función llamada substract que toma dos parámetros para restar
     return a - b;
 }
 
-function multiply(a,b) {
+function multiply(a,b) {//función llamada multiply que toma dos parámetros para multiplicar
     return a * b;
 }
 
-function divide(a,b) {
+function divide(a,b) {//función llamada divide que toma dos parámetros para dividir
     if (b == 0) {
-        return "Error: no se puede dividir entre cero"
+        return "Error: no se puede dividir entre cero"// si b es igual a cero, devuelve un mensaje de error indicando que no se puede dividir entre cero
     } else {
         return a / b;
     }
 
 }
 
-Math.add = add;
-Math.substract = substract;
-Math.multiply = multiply;
-Math.divide = divide;
+Math.add = add;// esta funcion se asigna como propiedad al objeto Math
+Math.substract = substract;// esta funcion se asigna como propiedad al objeto Math
+Math.multiply = multiply;// esta funcion se asigna como propiedad al objeto Math
+Math.divide = divide;// esta funcion se asigna como propiedad al objeto Math
 
-module.exports = Math;
+module.exports = Math;//se exporta Math para importarlo en el archivo app.js
